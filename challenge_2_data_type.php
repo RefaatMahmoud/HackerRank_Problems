505ef57d235c091bc14f6b4fe3efb4fddca10963
RefaatMahmoud <RefaatAishDev@gmail.com>
2020-05-14 19:31:47 +0200

    Challenge Name : Data Types

---
+++ challenge_2_data_type.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Challenge Name : Data Types
+ * Challenge Link : https://www.hackerrank.com/challenges/30-data-types
+ * */
+
+$handle = fopen ("php://stdin","r");
+$i = 4;
+$d = 4.0;
+$s = "HackerRank ";
+
+// Declare second integer, double, and String variables.
+$x =  (int)readline();
+$y = (float)readline();
+$z = (string)readline();
+
+// Read and save an integer, double, and String to your variables.
+$i += $x;
+$d += $y;
+$s .= $z;
+
+// Print the sum of both integer variables on a new line.
+print($i . "\n");
+// Print the sum of the double variables on a new line.
+print(number_format((float)$d, '1', '.', '') . "\n");
+// Concatenate and print the String variables on a new line
+print($s . "\n");
+
+
+
+fclose($handle);
+?>
+
